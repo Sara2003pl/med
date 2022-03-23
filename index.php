891b3b73bfcb2d8aa60b3cfc67e74169f6b66f42
krzeptowska <raimenee@gmail.com>
2022-03-23 11:06:03 +0100

    hehe

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
-$test = 1212;
-echo "siema swiat";
+$db = new mysqli("localhost", "root", "", "med");
 
+$q = $db->prepare("SELECT * FROM staff");
+if($q->execute()) {
+
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()){
+
+        $id = $row['ID'];
+        $firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+        echo "lekarz $firstName $lastName<br>";
+
+    }
+} else {
+    die("blad pobierania z bazy danych");
+}
 ?>
