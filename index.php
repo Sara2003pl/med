f51c4579120d39403239820306bc335d5d960510
krzeptowska <raimenee@gmail.com>
2022-03-23 12:01:10 +0100

    aha

--- index.php
+++ index.php
@@ -3,15 +3,32 @@
 $db = new mysqli("localhost", "root", "", "med");
 
 $q = $db->prepare("SELECT * FROM staff");
-if($q->execute()) {
+if($q && $q->execute()) {
 
     $result = $q->get_result();
-    while($row = $result->fetch_assoc()){
+    while($staff = $result->fetch_assoc()){
 
-        $id = $row['ID'];
-        $firstName = $row['firstName'];
-        $lastName = $row['lastName'];
+        $staffId = $staff['ID'];
+        $firstName = $staff['firstName'];
+        $lastName = $staff['lastName'];
         echo "lekarz $firstName $lastName<br>";
+        $q = $db->prepare("SELECT * FROM appointment WHERE staff_id = ?");
+        $q->bind_param("i", $staffId);
+        if($q && $q->execute()) {
+
+            $appointments = $q->get_result();
+            while($appointment = $appointments->fetch_assoc()) {
+                $appointmentId = $appointment['ID'];
+                $appointmentData = $appointment['data'];
+                $appointmentTimestamp = strtotime($appointmentData);
+                echo "<button>";
+                echo date("j.m H:i", $appointmentTimestamp);
+                echo "</button>";
+            }
+
+        } else {
+            die("blad pobrania wizyty z bazy danych");
+        }
 
     }
 } else {
